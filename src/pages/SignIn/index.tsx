f8a3b18a2ecacb7c06093db6c971020ecad90627
Danilo Bandeira <danilobandeira29@gmail.com>
2020-07-21 15:48:18 -0300

    feat: add new features to the SignIn page
    add ForgotPasswordButton, CreateAccountButton and KeyboardAvoidingView/ScrollView to usability

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,18 +1,50 @@
 import React from 'react';
+import { ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
 
+import Icon from 'react-native-vector-icons/Feather';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPasswordButton,
+  ForgotPasswordButtonText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Title>Sign In</Title>
-      <Input placeholder="Name" icon="mail" />
-      <Input placeholder="Password" icon="lock" />
-      <Button onPress={() => console.log('acessar')}>Acessar</Button>
-    </Container>
+    <>
+      <KeyboardAvoidingView
+        enabled
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      >
+        <ScrollView
+          contentContainerStyle={{ flexGrow: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Title>Sign In</Title>
+            <Input placeholder="Name" icon="mail" />
+            <Input placeholder="Password" icon="lock" />
+            <Button onPress={() => console.log('acessar')}>Access</Button>
+            <ForgotPasswordButton>
+              <ForgotPasswordButtonText>
+                Forgot my password
+              </ForgotPasswordButtonText>
+            </ForgotPasswordButton>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <CreateAccountButton>
+        <Icon name="log-in" size={20} color="#e02041" />
+        <CreateAccountButtonText>Create Account</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   );
 };
 
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,14 +1,54 @@
 import styled from 'styled-components/native';
+import { Platform } from 'react-native';
+
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-  padding: 20px;
+  padding: 0 30px ${Platform.OS === 'android' ? '70' : '40'}px;
 `;
 
 export const Title = styled.Text`
   font-size: 24px;
   font-family: 'RobotoSlab-Medium';
   margin-bottom: 30px;
+`;
+
+export const ForgotPasswordButton = styled.TouchableOpacity`
+  width: 100%;
+  background-color: transparent;
+  align-items: center;
+  justify-content: center;
+  height: 60px;
+  margin-top: 20px;
+`;
+export const ForgotPasswordButtonText = styled.Text`
+  color: #333;
+  font-size: 16px;
+  font-family: 'RobotoSlab-Regular';
+`;
+
+export const CreateAccountButton = styled.TouchableOpacity`
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 16px 0 ${16 + getBottomSpace()}px;
+
+  border-top-width: 1px;
+  border-style: solid;
+  border-top-color: #333;
+`;
+
+export const CreateAccountButtonText = styled.Text`
+  margin-left: 10px;
+
+  font-size: 16px;
+  font-family: 'RobotoSlab-Regular';
+  color: #333;
 `;
